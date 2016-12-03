07238c0c71b61728933a770b0fc63299a0941af8
Doug Riblet <smashzen85@gmail.com>
2016-12-03 12:08:37 -0600

    fix x y h w confusion

--- client/components/Dungeon.jsx
+++ client/components/Dungeon.jsx
@@ -16,7 +16,7 @@ export default class Dungeon extends React.Component {
     this.newDun = Array.from({length: 80}, () => Array.from({length: 120}, () => 'rock'))
     let count = 0
     // fill up blank dungeon with rooms
-    while (count < 100) {
+    while (count < 200) {
       this.generateRoom()
       count++
     }
@@ -29,12 +29,14 @@ export default class Dungeon extends React.Component {
     let rw = 6 + Math.floor(Math.random() * 6)
     let rh = 6 + Math.floor(Math.random() * 6)
     // make Room position
-    let rx = Math.floor(Math.random() * (120 - rw))
-    let ry = Math.floor(Math.random() * (80 - rh))
+    let rx = Math.floor(Math.random() * (119 - rw))
+    let ry = Math.floor(Math.random() * (79 - rh))
+
+    console.log('w,h,x,y: ', rw, rh, rx, ry)
     // check if position empty
     let empty = true
-    for (let row = ry; row <= ry + rw; row++) {
-      for (let col = rx; col <= rx + rh; col++) {
+    for (let row = ry; row <= ry + rh; row++) {
+      for (let col = rx; col <= rx + rw; col++) {
         if (this.newDun[row][col] !== 'rock') {
           empty = false
           break
@@ -42,8 +44,8 @@ export default class Dungeon extends React.Component {
       }
     }
     if (empty) {
-      for (let row = ry + 1; row < ry + rw; row++) {
-        for (let col = rx + 1; col < rx + rh; col++) {
+      for (let row = ry + 1; row < ry + rh; row++) {
+        for (let col = rx + 1; col < rx + rw; col++) {
           this.newDun[row][col] = 'open'
         }
       }
@@ -58,7 +60,19 @@ export default class Dungeon extends React.Component {
     return (
       <div>
         <svg>
-
+          {
+            this.state.grid.map((row, gy) => {
+              return row.map((dot, gx) => {
+                let oxo = 'on' + String(dot)
+                return (
+                  <rect
+                    className={oxo}
+                    x={gx * 10} y={gy * 10}
+                  />
+                )
+              })
+            })
+          }
         </svg>
       </div>
     )
